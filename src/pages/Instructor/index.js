972e017cf8575c9ca6a10ccefb1713198944c834
Natalia Nunes <naticristinenunes@gmail.com>
2021-11-15 16:54:09 -0300

    fix:corrigido icone

--- src/pages/Instructor/index.js
+++ src/pages/Instructor/index.js
@@ -2,12 +2,16 @@ import Carrousel from "../../components/Carousel";
 import { Container } from "./styles";
 import Julio from "../../asserts/Professor/Julio.jpeg";
 import Milton from "../../asserts/Professor/Milton.jpeg";
+import paulo from "../../asserts/Professor/paulo.jpeg";
+import moises from "../../asserts/Professor/moises.jpeg";
 import Card from "../../components/Card";
 
 const Professor = () => {
   const instructores = [
     { image: Julio, name: "Julio", graducao: "Faixa-Preta" },
     { image: Milton, name: "Milton", graducao: "Faixa-Preta" },
+    { image: paulo, name: "Paulo", graducao: "Faixa-Preta" },
+    { image: moises, name: "Moises", graducao: "Faixa-Preta" },
   ];
 
   return (
